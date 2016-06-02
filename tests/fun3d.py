af462dbebb27dc80e3a16313d4c464058d0a0550
Qiqi Wang <qiqi@mit.edu>
2016-06-02 16:46:54 -0400

    Debug by Eric and Bill Jones

--- tests/fun3d.py
+++ tests/fun3d.py
@@ -6,9 +6,9 @@ import tempfile
 from subprocess import *
 from multiprocessing.pool import ThreadPool
 
-import matplotlib
+#import matplotlib
 #matplotlib.use('Agg')
-from pylab import *
+#from pylab import *
 from numpy import *
 
 my_path = os.path.dirname(os.path.abspath(__file__))
@@ -18,7 +18,7 @@ from fds import finite_difference_shadowing
 
 def lift_drag_from_text(text):
     lift_drag = []
-    for line in text:
+    for line in text.split('\n'):
         line = line.strip().split()
         if len(line) == 4 and line[0] == 'Lift' and line[2] == 'Drag':
             lift_drag.append([line[1], line[3]])
@@ -31,34 +31,43 @@ def solve(u0, mach, nsteps, run_id):
     work_path = os.path.join(base_path, run_id)
     initial_data_file = os.path.join(work_path, 'initial.data.0')
     final_data_file = os.path.join(work_path, 'final.data.0')
-    lift_drag_file = os.path.join(work_path, 'lift_drag.txt'))
+    lift_drag_file = os.path.join(work_path, 'lift_drag.txt')
     if not os.path.exists(final_data_file):
         if not os.path.exists(work_path):
             os.mkdir(work_path)
+        shutil.copy(os.path.join(my_path,'fun3d.nml'),work_path)
+        shutil.copy(os.path.join(my_path,'rotated.b8.ugrid'),work_path)
+        shutil.copy(os.path.join(my_path,'rotated.mapbc'),work_path)
         with open(initial_data_file, 'wb') as f:
             f.write(asarray(u0, dtype='>d').tobytes())
+#       screen_output = check_output([
+#           'mpiexec', fun3d_bin, '--read_initial_field', '--write_final_field',
+#           '--xmach', str(mach), '--ncyc', str(nsteps)
+#       ], cwd=work_path)
         screen_output = check_output([
-            fun3d_bin, '--read_initial_field', '--write_final_field',
-            '--xmach', str(mach), '--ncyc', str(nsteps)
-        ], cwd=work_path)
+            'mpiexec', fun3d_bin, '--write_final_field', '--ncyc', str(nsteps)], cwd=work_path)
+        print screen_output
+        with open(os.path.join(work_path,'flow.output'),'w') as outfile:
+          outfile.write(screen_output)
         savetxt(lift_drag_file, lift_drag_from_text(screen_output))
     J = loadtxt(lift_drag_file)
+    print len(J), nsteps
     assert len(J) == nsteps
     with open(final_data_file, 'rb') as f:
         u1 = frombuffer(f.read(), dtype='>d')
     return ravel(u1), J
 
 # modify to point to fun3d binary
-fun3d_bin = os.path.join(my_path, '..', '..', 'fun3d', 'fun3d')
+fun3d_bin = os.path.join(os.sep,'u','enielsen','GIT','Master','fun3d','optimized','FUN3D_90','nodet_mpi')
 
 Ji, Gi = finite_difference_shadowing(
             solve,
-            random.rand(10000 * 5), # change 10000 to num of CV
-            0.25,                   # nominal xmach parameter
+            random.rand(116862 * 5), # change 10000 to num of CV
+            0.1,                    # nominal xmach parameter
             30,                     # number of unstable modes
             50,                     # number of time chunks
             1000,                   # number of time steps per chunk
-            1000,                   # run-up time steps
+            10,                     # run-up time steps
             epsilon=1E-4,
             verbose=True
          )
