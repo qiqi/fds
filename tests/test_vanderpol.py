d94ec331b6da30e46bd4fe7738d657e3ec620a0f
Qiqi Wang <qiqi@mit.edu>
2016-06-11 23:49:29 -0400

    html tutorial

---
+++ tests/test_vanderpol.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import shutil
+import tempfile
+from subprocess import *
+
+from numpy import *
+
+my_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(my_path, '..'))
+
+from fds import *
+
+solver_path = os.path.join(my_path, 'solvers', 'vanderpol')
+solver = os.path.join(solver_path, 'solver')
+u0 = loadtxt(os.path.join(solver_path, 'u0'))
+
+def solve(u, s, nsteps):
+    tmp_path = tempfile.mkdtemp()
+    with open(os.path.join(tmp_path, 'input.bin'), 'wb') as f:
+        f.write(asarray(u, dtype='>d').tobytes())
+    with open(os.path.join(tmp_path, 'param.bin'), 'wb') as f:
+        f.write(asarray(s, dtype='>d').tobytes())
+    call([solver, str(int(nsteps))], cwd=tmp_path)
+    with open(os.path.join(tmp_path, 'output.bin'), 'rb') as f:
+        out = frombuffer(f.read(), dtype='>d')
+    with open(os.path.join(tmp_path, 'objective.bin'), 'rb') as f:
+        J = frombuffer(f.read(), dtype='>d')
+    shutil.rmtree(tmp_path)
+    return out, J
+
+#if __name__ == '__main__':
+def test_gradient():
+    J, G = shadowing(solve, u0, 1, 1, 10, 500, 2000)
+    assert 1.5 < J < 2.5
+    assert 0.05 < G < 0.15
