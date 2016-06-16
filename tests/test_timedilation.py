490e0b0fd8d788e6ce147f98a1c3ca7017528847
Qiqi Wang <qiqi@mit.edu>
2016-06-16 08:40:34 -0400

    Fixed integer / float problem in factorial  in test_timedilation

--- tests/test_timedilation.py
+++ tests/test_timedilation.py
@@ -8,10 +8,19 @@ sys.path.append(os.path.join(my_path, '..'))
 from fds import timedilation
 
 def factorial(k):
-    return 1 if k <= 1 else k * factorial(k-1)
+    return 1.0 if k <= 1 else k * factorial(k-1)
 
 def test_monomial():
     for k in range(2, 10):
         x = arange(k+1) ** k / factorial(k)
-        timedilation.set_order_of_accuracy(k)
         assert abs(timedilation.compute_dxdt(x)) < 1E-12
+
+#def test_sine():
+# if __name__ == '__main__':
+#     dydx = []
+#     for k in range(2, 10):
+#         dydx.append([])
+#         for dx in [0.1, 0.01, 0.001, 0.0001]:
+#             y = sin(arange(k + 1) * dx)
+#             dydx[-1].append(timedilation.compute_dxdt(y) / dx)
+#     error = array(dydx) - 1
