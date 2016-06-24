ee9e3f76f09f3a05eb3d91dea7510ca83348bde8
Qiqi Wang <qwang@radix.trade>
2016-06-24 08:12:57 -0500

    Added solve_dim

--- fds/lsstan.py
+++ fds/lsstan.py
@@ -34,7 +34,25 @@ class LssTangent:
         D = sparse.bsr_matrix((Rs, r_[:nseg], r_[:nseg+1]), shape=matrix_shape)
         B = (D - I).tocsr()
         alpha = -(B.T * splinalg.spsolve(B * B.T, ravel(bs)))
+        # alpha1 = splinalg.lsqr(B, ravel(bs), iter_lim=10000)
         return alpha.reshape([nseg+1,-1])[:-1]
+
+    def solve_dim(self, dim):
+        Rs, bs = array(self.Rs), array(self.bs)
+        Rs11, bs1 = Rs[:,dim:,dim:], bs[:,dim:]
+        alpha = zeros_like(bs)
+        for i in range(len(bs)-1):
+            alpha[i+1,dim:] = dot(Rs11[i], alpha[i,dim:]) + bs1[i]
+            bs[i] -= alpha[i+1] - dot(Rs[i], alpha[i])
+        Rs00, bs0 = Rs[:,:dim,:dim], bs[:,:dim]
+        nseg, subdim = bs0.shape
+        eyes = eye(subdim, subdim) * ones([nseg, 1, 1])
+        matrix_shape = (subdim * nseg, subdim * (nseg+1))
+        I = sparse.bsr_matrix((eyes, r_[1:nseg+1], r_[:nseg+1]))
+        D = sparse.bsr_matrix((Rs00, r_[:nseg], r_[:nseg+1]), shape=matrix_shape)
+        B = (D - I).tocsr()
+        a = -(B.T * splinalg.spsolve(B * B.T, ravel(bs0)))
+        alpha[:,:dim] = a.reshape([nseg+1,-1])[:-1]
 
     def lyapunov_exponents(self):
         Rs = array(self.Rs)
