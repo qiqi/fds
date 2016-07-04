2362d857897dd8a676b5ab18aaee3c91dc1a1cf4
talnikar <talnikar@mit.edu>
2016-07-03 23:52:48 -0400

    serial adFVM working

--- apps/adFVM.py
+++ apps/adFVM.py
@@ -1,66 +1,89 @@
 import os
+import sys
 import subprocess
 import numpy as np
+import shutil
 
+import config
+config.hdf5 = True
 from pyRCF import RCF
 from field import IOField
 import h5py
 
-case = 'cases/cylinder/'
-program = '/home/talnikar/adFVM/problem.py'
+prefix = '/home/talnikar/adFVM/'
+case = prefix + 'cases/cylinder/'
+program = prefix + 'problem.py'
 problem = 'cylinder.py'
-nProcessors = 2
+nProcessors = 1
 time = 0.0
 solver = RCF(case)
 mesh = solver.mesh
+meshO = mesh.origMesh
 
-def runCase(initFields, parameters, nSteps, runID, _):
+def getInternalFields(time):
+    fields = solver.initFields(time)
+    fields = solver.stackFields(fields, np)
+    return fields[:meshO.nInternalCells]
+
+def runCase(initFields, parameters, nSteps, run_id):
 
     # generate case folders
-    case = case + '/'
-    caseDir = '{}{}/'.format(case, runID)
-    os.mkdir(caseDir)
-    shutil.copyFile(case + problem, caseDir)
-    shutil.copyTree(case + 'mesh.hdf5', caseDir)
-    shutil.copyTree(case + '0.hdf5', caseDir)
-    problem = caseDir + problem
+    caseDir = '{}/temp/{}/'.format(case, run_id)
+    mesh.case = caseDir
+    if not os.path.exists(caseDir):
+        os.mkdir(caseDir)
+    problemFile = caseDir + problem
+    shutil.copyfile(case + problem, problemFile)
+    shutil.copyfile(case + 'mesh.hdf5', caseDir + 'mesh.hdf5')
+    shutil.copyfile(case + '0.hdf5', caseDir + '0.hdf5')
+    outputFile = caseDir  + 'output.log'
 
     # write initial field
     #initFields = distributeData(initGlobalFields)
+    #sys.stdout = os.devnull
     fields = solver.unstackFields(initFields, IOField)
     fields = solver.primitive(*fields)
-    mesh.case = case
     IOField.openHandle(time)
     for phi in fields:
         phiO = IOField.read(phi.name)
-        phiO.field[:mesh.nInternalCells] = phi.field
+        phiO.partialComplete()
+        phiO.field[:meshO.nInternalCells] = phi.field
+        phiO.write()
     IOField.closeHandle()
-    mesh.case = caseDir
-    IOField.openHandle(time)
-    for phi in fields:
-        phi.write()
-    IOField.closeHandle()
+    #sys.stdout = sys.__stdout__
 
     # modify problem.py
-    with open(problem, 'r') as f:
+    with open(problemFile, 'r') as f:
         lines = f.readlines()
-    with open(problem, w) as f:
+    with open(problemFile, 'w') as f:
         for line in lines:
             writeLine = line.replace('NSTEPS', str(nSteps))
-            writeLine = line.replace('PARAMETERS', parameters)
+            writeLine = writeLine.replace('STARTTIME', str(time))
+            writeLine = writeLine.replace('CASEDIR', '\'{}\''.format(caseDir))
+            #writeLine = line.replace('PARAMETERS', parameters)
             f.write(writeLine)
 
-    subprocess.Popen(['mpirun', '-np', str(nProcessors),
-                      program, problem])
+    with open(outputFile, 'w') as f:
+        returncode = subprocess.call(['mpirun', '-np', str(nProcessors),
+                          program, problemFile, 'orig'],
+                          stdout=f, stderr=f)
+    if returncode:
+        raise Exception('Execution failed, check error log:', outputFile)
 
     # read final fields
-    fields = solver.initFields(lastTime)
-    fields = solver.stackFields(fields, np)
-    finalFields = fields[:mesh.nInternalCells]
-
+    lastTime = mesh.getTimes()[-1]
+    finalFields = getInternalFields(lastTime)
     # read objective values
     objectiveSeries = np.loadtxt(caseDir + 'timeSeries.txt')
 
     return finalFields, objectiveSeries
 
 from fds import shadowing
+
+u0 = getInternalFields(time)
+parameters = 1.0
+dims = 2
+segments = 10
+steps = 10
+#runCase(u0, parameters, steps, 'random')
+shadowing(runCase, u0, parameters, dims, segments, steps, 0)
