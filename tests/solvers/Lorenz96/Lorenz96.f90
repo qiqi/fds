d4b3f4f1b5711366b2639b3820e6cf9c0e29a1a1
Nisha Chandramoorthy <nishac92@gmail.com>
2016-10-16 18:54:47 -0400

    Extent perturbation vector.

--- tests/solvers/Lorenz96/Lorenz96.f90
+++ tests/solvers/Lorenz96/Lorenz96.f90
@@ -33,76 +33,81 @@ subroutine dfdX(X,D,dfdX_res)
 	real(kind=8), intent(in), dimension(D) :: X
 	real(kind=8), intent(out), dimension(D-3,D-3) :: dfdX_res 	
 	integer :: i,j
+
 	
 	dfdX_res = 0.d0
-	do i=3,D-1
-		j = i - 2	
+	do j=3,D-4
+		i = j + 2	
 		dfdX_res(j,j) = -1.d0
 		dfdX_res(j,j-1) = -X(i-2) + X(i+1)
 		dfdX_res(j,j+1) = X(i-1)
 		dfdX_res(j,j-2) = -X(i-1)  					
 
-	enddo		
-!	dfdX_res(1,D) = X(2) - X(D-1)
-!	dfdX_res(1,D-1) = -X(D)
-!	dfdX_res(1,1) = -1.d0
-!	dfdX_res(1,2) = X(D)
+	enddo	
+
+	dfdX_res(1,1) = -1.d0
+	dfdX_res(1,2) = X(2)
+	dfdX_res(2,2) = -1.d0	 	
+	dfdX_res(2,1) = X(5) - X(2)
+	dfdX_res(2,3) = X(3)	
 	
-	    		
-!	dfdX_res(2,1) = X(3) - X(D)
-!	dfdX_res(2,D) = -X(1)
-!	dfdX_res(2,2) = -1.d0
-!	dfdX_res(2,3) = X(1)
-	
-!	dfdX_res(D,D-1) = X(1) - X(D-2)
-!	dfdX_res(D,D-2) = -X(D-1)
-!	dfdX_res(D,D) = -1.d0
-!	dfdX_res(D,1) = X(D-1)
+	dfdX_res(D-3,D-3) = -1.d0
+	dfdX_res(D-3,D-4) = X(D) - X(D-3)
+	dfdX_res(D-3,D-5) = -X(D-2) 
 
 end subroutine dfdX  
 
-subroutine dvdt(X,D,v,dvdt_res)
+subroutine dvdt(X,D,v1,dvdt_res)
 
 		implicit none
 		integer, intent(in) :: D
 		real(kind=8), intent(in), dimension(D) :: X
-		real(kind=8), intent(in), dimension(D-3) :: v
 		real(kind=8), intent(out), dimension(D-3,1) :: dvdt_res
 		real(kind=8), dimension(D-3,D-3) :: dfdX_res
-		real(kind=8), dimension(D-3,1) :: v1
+		real(kind=8), intent(in), dimension(D,1) :: v1
+		real(kind=8), dimension(D-3,D) :: dfdX_ext
 		
 		
 		call dfdX(X,D,dfdX_res)
-		v1 = reshape(v,[D-3,1])
-		dvdt_res = matmul(dfdX_res,v1)	
+		dfdX_ext = 0.d0
+		dfdX_ext(1:D-3,3:D-1) = dfdX_res
+	
+		dfdX_ext(1,2) = X(4) - X(1)
+		dfdX_ext(1,1) = -X(2)  				
+	
+		dfdX_ext(2,1) = -X(3) 	
+		dfdX_ext(D-3,D) = X(D-2) 
+		
+		dvdt_res = matmul(dfdX_ext,v1)	
 		 	
 end subroutine dvdt
-subroutine rk4(X,D,v,vnp1)
+subroutine rk45(X,D,v,vnp1)
 
 	implicit none
 	integer, intent(in) :: D
-	real(kind=8) , intent(in), dimension(D,1) :: X
-	real(kind=8) , intent(in), dimension(D-3,1) :: v
+	real(kind=8) , intent(in), dimension(D) :: X,v
 	real(kind=8) , intent(out), dimension(D-3,1) :: vnp1
-	real(kind=8) , dimension(D-3,1) :: dvdt_res,k1,k2,k3,k4
+	real(kind=8) , dimension(D-3,1) :: v1,dvdt_res,k1,k2,k3,k4
 	real(kind=8) :: dt
+
 	
 
 	dt = 0.001d0	
-	call dvdt(X,D,v,dvdt_res)
+	v1 = reshape(v,[D-3,1])
+	call dvdt(X,D,v1,dvdt_res)	
 	k1 = dt*dvdt_res
-	call dvdt(X,D,v + 0.5d0*k1,dvdt_res)
+	call dvdt(X,D,v1 + 0.5d0*k1,dvdt_res)
 	k2 = dt*dvdt_res
-	call dvdt(X,D,v + 0.5d0*k2,dvdt_res)
+	call dvdt(X,D,v1 + 0.5d0*k2,dvdt_res)
 	k3 = dt*dvdt_res
-	call dvdt(X,D,v + k3,dvdt_res)
+	call dvdt(X,D,v1 + k3,dvdt_res)
 	k4 = dt*dvdt_res
 
-	vnp1 = v + 1.d0/6.d0*k1 + &
+	vnp1 = v1 + 1.d0/6.d0*k1 + &
 		 1.d0/3.d0*k2 + 1.d0/3.d0*k3 + &
 		1.d0/6.d0*k4
 	
 
-end subroutine rk4 
+end subroutine rk45 
 
 end module Lorenz96
--- tests/solvers/Lorenz96/test1.f90
+++ tests/solvers/Lorenz96/test1.f90
@@ -6,20 +6,21 @@ program test1
     use mpi 
 
     implicit none
-	real(kind=8), dimension(:), allocatable ::X,v,vnp1_res, Xnp1_res
+	real(kind=8), dimension(:), allocatable ::X,v, Xnp1_res
 	real(kind=8), dimension(:), allocatable :: dXdt
-	real(kind=8), dimension(:,:), allocatable :: dfdX_res
+	real(kind=8), dimension(:,:), allocatable :: dfdX_res, vnp1_res
 	integer :: i, me, ierr, nprocs, Dproc, D
     integer :: istart, iend, ncyc, lproc, rproc	
 	integer, allocatable :: seed(:)
 	integer :: rsize, req1, req2
 	integer, dimension(MPI_STATUS_SIZE) :: mpistatus
+ 	real(kind=8), pointer, dimension(:) :: p	
     call mpi_init(ierr)
     call mpi_comm_size(MPI_COMM_WORLD, nprocs, ierr)
     call mpi_comm_rank(MPI_COMM_WORLD, me, ierr)
 
 	ncyc = 1
-	D = 40
+	D = 8
 	Dproc =	D/nprocs	
 
 	istart = me*Dproc + 1
@@ -27,7 +28,7 @@ program test1
 	lproc = MOD(me + nprocs - 1, nprocs)
 	rproc = MOD(me + 1, nprocs)
 
-	allocate(X(istart-2:iend+1),v(istart:iend),vnp1_res(istart:iend),Xnp1_res(istart:iend))
+	allocate(X(istart-2:iend+1),v(istart-2:iend+1),vnp1_res(istart:iend,1),Xnp1_res(istart:iend))
 
 	call RANDOM_SEED(SIZE=rsize)
 	allocate(seed(rsize))
@@ -37,6 +38,8 @@ program test1
 	if(me == 0) then 
 		v(istart) = 1.d0
 	end if
+	print *, me, X
+
 	do i = 1, ncyc	
 
 		call mpi_isend(X(istart), 1,  &
@@ -57,13 +60,39 @@ program test1
 		call mpi_recv(X(iend+1), &
 		1, MPI_DOUBLE_PRECISION, rproc, &
 	    1, MPI_COMM_WORLD, mpistatus, ierr)			 			
-		call Xnp1(X,D+3,Xnp1_res)
 
+		call mpi_isend(v(istart), 1,  &
+		MPI_DOUBLE_PRECISION,         &
+		lproc,  &
+		3, MPI_COMM_WORLD, req1, ierr)
+			
+		call mpi_isend(v(iend-1:iend), &
+		2, MPI_DOUBLE_PRECISION,       &
+		rproc,   &
+		4, MPI_COMM_WORLD, req2, ierr)
+
+
+		call mpi_recv(X(istart-2:istart-1), &
+		2, MPI_DOUBLE_PRECISION, lproc, &
+	    4, MPI_COMM_WORLD, mpistatus, ierr)			 			
+		
+		call mpi_recv(X(iend+1), &
+		1, MPI_DOUBLE_PRECISION, rproc, &
+	    3, MPI_COMM_WORLD, mpistatus, ierr)		
+
+
+		
+		call Xnp1(X,Dproc+3,Xnp1_res)
+		call rk45(X,Dproc+3,v,vnp1_res)
+
+		if(me == 0) then
+		!Compute lift and drag.	
+		end if
 		X(istart:iend) = Xnp1_res
 		
-		!v(istart:iend) = vnp1
+		v(istart:iend) = vnp1_res(:,1)
 							
-		!call rk4(X,D+3,v,vnp1)		
+		
 		
 	enddo
 	call mpi_finalize(ierr)	
