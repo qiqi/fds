17ab657336ad34bfb5ff4f6d0889dfc1311cfd63
Nisha Chandramoorthy <nishac92@gmail.com>
2016-10-18 10:55:45 -0400

    Ensemble Tangent skeleton

--- tests/solvers/Lorenz96/test1.f90
+++ tests/solvers/Lorenz96/test1.f90
@@ -16,12 +16,13 @@ program test1
 	integer, dimension(MPI_STATUS_SIZE) :: mpistatus
  	real(kind=8), pointer, dimension(:) :: p
 	real(kind=8) :: dt	
+	integer :: thefile
     call mpi_init(ierr)
     call mpi_comm_size(MPI_COMM_WORLD, nprocs, ierr)
     call mpi_comm_rank(MPI_COMM_WORLD, me, ierr)
 
-	ncyc = 15000
-	D = 38
+	ncyc = 2000
+	D = 40
 	Dproc =	D/nprocs	
 	dt = 0.01d0
 	istart = me*Dproc + 1
@@ -41,7 +42,13 @@ program test1
 		v(istart) = 1.d0
 		Xp(istart) = Xp(istart) + 0.001d0
 	end if
-	print *, me, X
+
+!	call MPI_FILE_OPEN(MPI_COMM_WORLD, 'test', &
+!			MPI_MODE_WRONLY + MPI_MODE_CREATE, &
+!			MPI_INFO_NULL, thefile, ierr)
+!			open(unit=30+me, file='initialdata.dat')
+!			write(30+me,*) X(istart:iend)	
+!			close(30+me)	
 
 	
 	do i = 1, ncyc	
@@ -114,11 +121,11 @@ program test1
 		Xp(istart:iend) = Xpnp1_res
 		v(istart:iend) = vnp1_res(:,1)
 						
-		if(me==0) then	
+		if(me==0 .and. i==1000) then	
 			open(unit=20, file='X.dat')
 			write(20,*) X(istart:iend)	
 			open(unit=21, file='Xp.dat')
-			write(20,*) Xp(istart:iend)		
+			write(21,*) Xp(istart:iend)		
 		end if
 	enddo
 	close(20)
